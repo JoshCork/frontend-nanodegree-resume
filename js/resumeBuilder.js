1df1ed7dd1e53ba43a2f06b6279e17704686d80d
Joshua Cork <joshua.cork@gmail.com>
2014-11-26 15:59:18 -0700

    not sure what I changed.  this was a commit from changes I made yesterday afternoon.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,10 @@
+
+
+var awesomeThoughts = "I am Joshua Cork and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+var formattedName
+var formattedRole
+
+
+
+$("#main").append(funThoughts);
