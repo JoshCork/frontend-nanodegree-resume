3b5919f860ad7d4b2c29626f5f52c668ed5f1336
Joshua Cork <joshua.cork@gmail.com>
2014-11-28 18:23:13 -0700

    Cleaned up resumeBuilder.js per instructions.  also added to feflections.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,108 @@
-var awesomeThoughts = "I am Joshua Cork and I am AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
-var formattedName
-var formattedRole
+var bio = {
+    "name": "Joshua Cork",
+    "role": "web developer extrodinaire",
+    "contactInfo": {
+        "email": "joshua.cork@gmail.com",
+        "mobile": "602.903.5153",
+        "twitter": "@joshCork",
+        "gitHub": "@joshcork",
+        "location": "Gilbert, AZ",
+    },
+    "pictureURL": "http://photos-b.ak.instagram.com/hphotos-ak-xfa1/10632278_743885619011617_1494692464_n.jpg",
+    "welcomeMessage": "Hello, and welcome to my world.",
+    "skills": ["Business Intelligence", "Analytics", "Handsomeness", "Interpersonal Communication"]
+};
 
-$("#main").append(funThoughts);
+var work = {
+    "jobs": [{
+        "employer": "Intel Corporation",
+        "datesWorked": "August 2000 to June 2001",
+        "title": "School Year Intern - Webmaster",
+        "location": "Chandler, AZ",
+        "description": "Built websites and databases for the Logistics Organization."
+    }, {
+        "employer": "Intel Corporation",
+        "datesWorked": "June 2001 to December 2002",
+        "title": "Logistics Analyst",
+        "location": "Chandler, AZ",
+        "description": "Responsible for transportation forecasting of a $300M transportation budget."
+    }, {
+        "employer": "Intel Corporation",
+        "datesWorked": "December 2002 to Present",
+        "title": "Information Solutions Engineering",
+        "location": "Chandler, AZ",
+        "description": "Worked my way up from data analyst to manager of the organization. Currently working as a scrum master and software apps engineer."
+    }],
+    "currentPosition": "scrum master",
+    "employer": "Intel Corporation",
+    "yearsWorked": "14",
+    "city": "Chandler, AZ"
+};
 
-var formattedName = "Joshua Cork";
-var formattedRole = "web developer extrodinaire";
 
-HTMLheaderName = HTMLheaderName.replace("%data%",formattedName)
-HTMLheaderRole = HTMLheaderRole.replace('%data%',formattedRole)
+var education = {
+    "schools": [{
+        "location": "Tempe, AZ",
+        "degree": "BS",
+        "majors": ["Computer Information Systems (CIS)"],
+        "minors": ["Accounting"],
+        "graduationYear": 2000,
+        "name": "Arizona State University",
+        "datesAttended": "August 1996 to May 2001"
+    }, {
+        "location": "Tempe, AZ",
+        "degree": "MS",
+        "majors": ["Information Management (MSIM)"],
+        "minors": ["n/a"],
+        "graduationYear": 2006,
+        "name": "Arizona State University",
+        "datesAttended": "June 2005 to May 2006"
+    }],
+    "onlineCourses": [{
+        "title": "JavaScript Basics",
+        "school": "Udacity",
+        "dates": 2014,
+        "url": "I need to find this"
+    }, {
+        "title": "Web Developer Nano-Degree",
+        "school": "Udacity",
+        "dates": 2014,
+        "url": "I need to find this"
+    }]
+};
 
+var projects = {
+    "projects": [{
+        "title": "The Sale Ring (eSaleRing.com)",
+        "datesWorked": "October 2014",
+        "description": "Created a landing page for The Sale Ring digial magazine.",
+        "images": ["URL of PROJECT IMAGES"]
+    }, {
+        "title": "GESDATE Landing Page (GESDATE.com)",
+        "datesWorked": "August 2014",
+        "description": "Created a landing page for a startup GESDATE.",
+        "images": ["URL of PROJECT IMAGES"]
+    }]
+};
+
+
+
+HTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+HTMLheaderRole = HTMLheaderRole.replace('%data%', bio.role);
+
+HTMLskills = HTMLskills.replace("%data%", bio.skills.join(", "));
+HTMLemail = HTMLemail.replace('%data%', bio.contactInfo.email);
+HTMLbioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+HTMLWelcomeMsg = HTMLWelcomeMsg.replace('%data%', bio.welcomeMessage);
+HTMLworkDescription = HTMLworkDescription.replace('%data%', work.currentPosition);
+HTMLschoolName = HTMLschoolName.replace('%data%', education.lastSchool);
+
+$("#header").prepend(HTMLWelcomeMsg);
 $("#header").prepend(HTMLheaderRole);
 $("#header").prepend(HTMLheaderName);
+
+$("#header").append(HTMLbioPic);
+$("#header").append(HTMLemail);
+$("#header").append(HTMLskills);
+$("#header").append(HTMLworkDescription);
+$("#header").append(HTMLschoolName);
--- project-2-reflections.md
+++ project-2-reflections.md
@@ -1,5 +1,30 @@
 project 2 reflections
 
+## 11/26/2014 - string manipulation.  
+Not sure if this is the most efficient way of doing this but created an array and filled it with words using the split function to split the original string based on spaces.  Then I assumed only two word names and made the first proper case using the slice trick and the last all uppercase using the uppercase function.  I used plunker and imported jQuery to push the HTML out to the browser where I could test and see my changes.  Here's my code: 
+
+	var finalName = "AlbERt EINstEiN";
+
+	var myStringArray = [];
+	myStringArray = finalName.split(" "); // splits the the final name string into separate strings by using the " ".
+
+	var firstName = myStringArray[0].toLowerCase();
+	var lastName = myStringArray[1].toUpperCase();
+	var firstLetter = firstName.charAt(0).toUpperCase();
+	finalName = firstLetter + firstName.slice(1) + " " + lastName;
+
+	var myOutput = finalName;
+
+
+	//firstName.slice(1)
+	/* will give me the first and last name now I need to make the first name proper case */ //firstName + " " + lastName;
+	/* finds the location of the space */  //oldName.indexOf(" ")
+	/* makes the entire oldName Uppercase*/ //oldName.toUpperCase();
+
+
+
+	$("#innerHTML").append(myOutput);
+
 ## 11/26/2014 - quiz troubles and understanding. 
 Took me a little bit to understand why my code was failing.  I was doing everything in the console and it was working fine.  I had to relize that they were passing a value into the function that i was supposed to use instead of their sample array.  Once I figured that out it was easy.  Also as you can see in the commented out code below I attempted this a harder way first.  
 
@@ -30,3 +55,5 @@ Took me a little bit to understand why my code was failing.  I was doing everyth
 
 	// Did your code work? The line below will tell you!
 	console.log(incrementLastArrayElement(sampleArray));
+
+
